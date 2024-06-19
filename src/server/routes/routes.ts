f9face807078f5e2d006456ad1cb68200d039ba8
Bryanzef <bryanzef7@gmail.com>
2024-06-19 20:45:48 -0300

    feat: add folder controllers

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -1,12 +1,15 @@
 import { StatusCodes } from "http-status-codes";
 import { Router } from "express";
+import { CidadesController } from "./../controllers";
 
 const router = Router();
 
-router.post("/teste", (req, res) => {
+router.get("/", (req, res) => {
     console.log(req.body);
 
-    return res.status(StatusCodes.ACCEPTED).json(req.body);
+    return res.send("Running");
 });
+
+router.post("/cidades", CidadesController.create);
 
 export { router };
