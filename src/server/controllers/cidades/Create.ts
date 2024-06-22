3e97545186dce57c0266d19f29359547c636aad5
Bryanzef <bryanzef7@gmail.com>
2024-06-21 21:12:43 -0300

    new folders

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,36 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
-interface Icidade {
+// Definindo a interface para a cidade
+interface ICidade {
     nome: string;
 }
 
-export const create = (req: Request<{}, {}, Icidade>, res: Response) => {
-    //
-    console.log(req.body);
+// Esquema de validação usando yup
+const bodyValidation: yup.ObjectSchema<ICidade> = yup
+    .object({
+        nome: yup.string().required().min(3),
+    })
+    .defined();
 
-    return res.send("Create");
+// Função para criar uma cidade
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validateData: ICidade | undefined = undefined;
+    try {
+        validateData = await bodyValidation.validate(req.body, { abortEarly: false });
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: {
+                default: yupError.message,
+            },
+        });
+    }
+
+    console.log(validateData);
+
+    // Resposta de sucesso
+    return res.status(StatusCodes.CREATED).json({ message: "Create" });
 };
